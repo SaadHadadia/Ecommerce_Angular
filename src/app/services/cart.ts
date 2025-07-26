74474c240de4e009bc0cc97ff704a7cb7a18f1e3
Saad Hadadia <saad.hadadia9@gmail.com>
2025-07-26 16:15:57 +0100

    Cart | localStorage

--- src/app/services/cart.ts
+++ src/app/services/cart.ts
@@ -1,12 +1,24 @@
-import { Injectable, signal } from '@angular/core';
+import { effect, Injectable, signal } from '@angular/core';
 import Product from '../components/models/products.models';
+
+const CART_STORAGE_KEY = 'app_cart';
 
 @Injectable({
   providedIn: 'root'
 })
 export class Cart {
 
-  cart = signal<Product[]>([]);
+  // Initialize signal with data from localStorage if available
+  private initialCart = this.loadCartFromStorage();
+  cart = signal<Product[]>(this.initialCart);
+
+  constructor() {
+    // Automatically persist to localStorage whenever the cart changes
+    effect(() => {
+      const cartValue = this.cart();
+      localStorage.setItem(CART_STORAGE_KEY, JSON.stringify(cartValue));
+    });
+  }
 
   addToCart(product: Product) {
     this.cart.set([...this.cart(), product]);
@@ -14,6 +26,15 @@ export class Cart {
 
   removeFromCart(productId: number) {
     this.cart.set(this.cart().filter((item) => item._id !== productId));
+  }
+
+  private loadCartFromStorage(): Product[] {
+    const stored = localStorage.getItem(CART_STORAGE_KEY);
+    return stored ? JSON.parse(stored) : [];
+  }
+
+  clearCart() {
+    this.cart.set([]);
   }
 
 }
