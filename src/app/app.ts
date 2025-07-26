2de5af308fc76a5fbdc8ca457fe6e6e8a19736bd
Saad Hadadia <saad.hadadia9@gmail.com>
2025-07-27 00:03:31 +0100

    Bug Fix | Header component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,22 +1,34 @@
 import { Component, signal } from '@angular/core';
 import { Header } from './components/header/header';
 import { RouterOutlet, Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [Header, RouterOutlet],
+  imports: [CommonModule, Header, RouterOutlet],
   template: `
-    <app-header *ngIf="showHeader()" />
-    <router-outlet />   
+    <app-header *ngIf="showHeader" />
+    <router-outlet />
   `,
   styles: [],
 })
 export class App {
-  protected readonly title = signal('project ddd');
+  protected readonly title = signal('project');
 
   constructor(private router: Router) { }
 
-  showHeader() {
-    return this.router.url !== '/not-found';
+  get showHeader(): boolean {
+    const currentUrl = this.router.url.split('?')[0].split('#')[0];
+    const routes = this.router.config;
+
+    return routes.some(route => {
+      if (route.path === '**') return false;
+
+      const pattern = '^/' + (route.path
+        ? route.path.replace(/:([^/]+)/g, '[^/]+').replace(/\//g, '\\/')
+        : '') + '$';
+
+      return new RegExp(pattern).test(currentUrl);
+    });
   }
 }
